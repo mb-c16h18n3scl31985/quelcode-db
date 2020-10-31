f3fa31953725e44352da8f6bc5985f1072438578
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-10-31 20:51:10 +0900

    chatroomsテーブルのDDL文作成

--- db/db-challenge2/chatroom_ddl.sql
+++ db/db-challenge2/chatroom_ddl.sql
@@ -11,3 +11,17 @@ CREATE TABLE users
     office_phone CHAR(13),
     mobile_phone CHAR(13)
 );
+
+CREATE TABLE chatrooms
+(
+    chatroom_id INTEGER PRIMARY KEY,
+    chatroom_name VARCHAR(100) NOT NULL,
+    description VARCHAR(1000),
+    is_enabled_file_upload TINYINT(1) DEFAULT 1 NOT NULL,
+    is_direct_chat TINYINT(1) DEFAULT 0 NOT NULL,
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
+    created_at DATETIME NOT NULL,
+    created_user_id INTEGER REFERENCES users(user_id) NOT NULL,
+    modified_at DATETIME NOT NULL,
+    modified_user_id INTEGER REFERENCES users(user_id) NOT NULL
+)
