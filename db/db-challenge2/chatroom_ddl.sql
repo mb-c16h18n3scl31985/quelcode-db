1020343292229516442782bd87f41565a03890a9
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-01 19:30:05 +0900

    コメントの追加

--- db/db-challenge2/chatroom_ddl.sql
+++ db/db-challenge2/chatroom_ddl.sql
@@ -7,7 +7,7 @@ CREATE TABLE users
     profile VARCHAR(1000),
     created_at DATETIME,
     modified_t DATETIME,
-    is_deleted TINYINT(1) DEFAULT 0,
+    is_deleted TINYINT(1) DEFAULT 0, -- 1:削除済み
     office_phone VARCHAR(13),
     mobile_phone VARCHAR(13)
 );
@@ -17,9 +17,9 @@ CREATE TABLE chatrooms
     chatroom_id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
     chatroom_name VARCHAR(100) NOT NULL,
     description VARCHAR(1000),
-    is_enabled_file_upload TINYINT(1) DEFAULT 1 NOT NULL,
-    is_direct_chat TINYINT(1) DEFAULT 0 NOT NULL,
-    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
+    is_enabled_file_upload TINYINT(1) DEFAULT 1 NOT NULL, -- 1:ファイルアップロード可能
+    is_direct_chat TINYINT(1) DEFAULT 0 NOT NULL, --  1:ダイレクトチャット
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL, -- 1:削除済み
     created_at DATETIME NOT NULL,
     created_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
     modified_at DATETIME NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE posts
     poseted_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
     post_content VARCHAR(1000) NOT NULL,
     attachment_name VARCHAR(100),
-    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL, -- 1:削除済み
     created_at DATETIME NOT NULL,
     modified_at DATETIME NOT NULL,
     modified_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL
@@ -54,8 +54,8 @@ CREATE TABLE tasks
     task_content VARCHAR(1000) NOT NULL,
     user_id_in_charge INTEGER REFERENCES users(user_id) NOT NULL,
     limit_date_and_time TIMESTAMP,
-    is_completed TINYINT(1) DEFAULT 0 NOT NULL,
-    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
+    is_completed TINYINT(1) DEFAULT 0 NOT NULL, -- 1:完了
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL, -- 1:削除済み
     poseted_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
     created_at DATETIME NOT NULL,
     modified_at DATETIME NOT NULL,
