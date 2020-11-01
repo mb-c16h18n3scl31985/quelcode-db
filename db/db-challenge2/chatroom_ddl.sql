a0b16737ba54e8839d8a7824caa99524609bf14d
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-01 19:23:19 +0900

    型修正、インデント

--- db/db-challenge2/chatroom_ddl.sql
+++ db/db-challenge2/chatroom_ddl.sql
@@ -1,46 +1,29 @@
 CREATE TABLE users
 (
     user_id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
-    mail VARCHAR
-(100) NOT NULL,
-    password VARCHAR
-(100) NOT NULL,
-    display_name VARCHAR
-(100),
-    profile VARCHAR
-(1000),
+    mail VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    display_name VARCHAR(100),
+    profile VARCHAR(1000),
     created_at DATETIME,
     modified_t DATETIME,
-    is_deleted TINYINT
-(1) DEFAULT 0,
-    office_phone VARCHAR
-(13),
-    mobile_phone VARCHAR
-(13)
+    is_deleted TINYINT(1) DEFAULT 0,
+    office_phone VARCHAR(13),
+    mobile_phone VARCHAR(13)
 );
 
 CREATE TABLE chatrooms
 (
-    chatroom_id INTEGER
-(11) PRIMARY KEY AUTO_INCREMENT,
-    chatroom_name VARCHAR
-(100) NOT NULL,
-    description VARCHAR
-(1000),
-    is_enabled_file_upload TINYINT
-(1) DEFAULT 1 NOT NULL,
-    is_direct_chat TINYINT
-(1) DEFAULT 0 NOT NULL,
-    is_deleted TINYINT
-(1) DEFAULT 0 NOT NULL,
+    chatroom_id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
+    chatroom_name VARCHAR(100) NOT NULL,
+    description VARCHAR(1000),
+    is_enabled_file_upload TINYINT(1) DEFAULT 1 NOT NULL,
+    is_direct_chat TINYINT(1) DEFAULT 0 NOT NULL,
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
     created_at DATETIME NOT NULL,
-    created_user_id INTEGER
-(11) REFERENCES users
-(user_id) NOT NULL,
+    created_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
     modified_at DATETIME NOT NULL,
-    modified_user_id INTEGER
-(11) REFERENCES users
-(user_id) NOT NULL
+    modified_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL
 );
 
 CREATE TABLE chatroom_members
@@ -54,46 +37,27 @@ CREATE TABLE chatroom_members
 CREATE TABLE posts
 (
     post_id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
-    posted_chatroom_id INTEGER
-(11) REFERENCES chatrooms
-(chatroom_id),
-    poseted_user_id INTEGER
-(11) REFERENCES users
-(user_id) NOT NULL,
-    post_content VARCHAR
-(1000) NOT NULL,
-    attachment_name VARCHAR
-(100),
-    is_deleted TINYINT
-(11) DEFAULT 0 NOT NULL,
+    posted_chatroom_id INTEGER(11) REFERENCES chatrooms(chatroom_id),
+    poseted_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
+    post_content VARCHAR(1000) NOT NULL,
+    attachment_name VARCHAR(100),
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
     created_at DATETIME NOT NULL,
     modified_at DATETIME NOT NULL,
-    modified_user_id INTEGER
-(11) REFERENCES users
-(user_id) NOT NULL
+    modified_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL
 );
 
 CREATE TABLE tasks
 (
     task_id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
-    chatroom_id INTEGER
-(11) REFERENCES chatrooms
-(chatroom_id),
-    task_content VARCHAR
-(1000) NOT NULL,
-    user_id_in_charge INTEGER REFERENCES users
-(user_id) NOT NULL,
+    chatroom_id INTEGER(11) REFERENCES chatrooms(chatroom_id),
+    task_content VARCHAR(1000) NOT NULL,
+    user_id_in_charge INTEGER REFERENCES users(user_id) NOT NULL,
     limit_date_and_time TIMESTAMP,
-    is_completed TINYINT
-(11) DEFAULT 0 NOT NULL,
-    is_deleted TINYINT
-(11) DEFAULT 0 NOT NULL,
-    poseted_user_id INTEGER
-(11) REFERENCES users
-(user_id) NOT NULL,
+    is_completed TINYINT(1) DEFAULT 0 NOT NULL,
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
+    poseted_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
     created_at DATETIME NOT NULL,
     modified_at DATETIME NOT NULL,
-    modified_user_id INTEGER
-(11) REFERENCES users
-(user_id) NOT NULL,
+    modified_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
 );
