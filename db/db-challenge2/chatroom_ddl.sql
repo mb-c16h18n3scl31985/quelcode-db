27286664f837695f6fc406dc5e0c3f9da80a5f92
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-03 17:44:54 +0900

    外部キー制約の修正

--- db/db-challenge2/chatroom_ddl.sql
+++ db/db-challenge2/chatroom_ddl.sql
@@ -21,43 +21,54 @@ CREATE TABLE chatrooms
     is_direct_chat TINYINT(1) DEFAULT 0 NOT NULL, --  1:ダイレクトチャット
     is_deleted TINYINT(1) DEFAULT 0 NOT NULL, -- 1:削除済み
     created_at DATETIME NOT NULL,
-    created_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
+    created_user_id INTEGER(11) NOT NULL,
     modified_at DATETIME NOT NULL,
-    modified_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL
+    modified_user_id INTEGER(11)  NOT NULL,
+    FOREIGN KEY chatroom_id REFERENCES chatrooms(id),
+    FOREIGN KEY user_id REFERENCES users(id),
 );
 
 CREATE TABLE chatroom_members
 (
-    chatroom_id INTEGER(11) REFERENCES chatrooms(chatroom_id) NOT NULL,
-    user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
+    chatroom_id INTEGER(11) NOT NULL,
+    user_id INTEGER(11) NOT NULL,
     joined_at DATETIME NOT NULL,
-    PRIMARY KEY(chatroom_id,user_id)
+    PRIMARY KEY(chatroom_id,user_id),
+    FOREIGN KEY chatroom_id REFERENCES chatrooms(id),
+    FOREIGN KEY user_id REFERENCES users(id)
 );
 
 CREATE TABLE posts
 (
     id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
-    chatroom_id INTEGER(11) REFERENCES chatrooms(chatroom_id),
+    chatroom_id INTEGER(11) NOT NULL,
     content VARCHAR(1000) NOT NULL,
     attachment_name VARCHAR(100),
     is_deleted TINYINT(1) DEFAULT 0 NOT NULL, -- 1:削除済み
     created_at DATETIME NOT NULL,
-    created_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
+    created_user_id INTEGER(11) NOT NULL,
     modified_at DATETIME NOT NULL,
-    modified_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL
+    modified_user_id INTEGER(11) NOT NULL,
+    FOREIGN KEY chatroom_id REFERENCES chatrooms(id),
+    FOREIGN KEY created_user_id REFERENCES users(id),
+    FOREIGN KEY modified_user_id REFERENCES users(id)
 );
 
 CREATE TABLE tasks
 (
     id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
-    chatroom_id INTEGER(11) REFERENCES chatrooms(chatroom_id),
+    chatroom_id INTEGER(11),
     content VARCHAR(1000) NOT NULL,
-    user_id INTEGER REFERENCES users(user_id) NOT NULL,
+    user_id INTEGER NOT NULL,
     limited_at DATETIME,
     is_completed TINYINT(1) DEFAULT 0 NOT NULL, -- 1:完了
     is_deleted TINYINT(1) DEFAULT 0 NOT NULL, -- 1:削除済み
     created_at DATETIME NOT NULL,
-    created_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
+    created_user_id INTEGER(11)  NOT NULL,
     modified_at DATETIME NOT NULL,
-    modified_user_id INTEGER(11) REFERENCES users(user_id) NOT NULL,
+    modified_user_id INTEGER(11) NOT NULL,
+    FOREIGN KEY chatroom_id REFERENCES chatrooms(id),
+    FOREIGN KEY user_id REFERENCES users(id),
+    FOREIGN KEY created_user_id REFERENCES users(id),
+    FOREIGN KEY modified_user_id REFERENCES users(id)
 );
