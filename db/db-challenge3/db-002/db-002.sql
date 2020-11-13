a588cb8ed6a30ab12b1f9e0a5c07a729a8f0cffe
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-13 19:35:33 +0900

    db-002 INSERT文大枠作成

---
+++ db/db-challenge3/db-002/db-002.sql
@@ -0,0 +1,12 @@
+INSERT INTO chatrooms
+    (name,
+    description,
+    is_enabled_file_upload,
+    is_direct_chat,
+    is_deleted,
+    created_user_id,
+    created_at,
+    modified_user_id,
+    modified_at,
+    )
+VALUES()
