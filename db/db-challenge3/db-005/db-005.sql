2f3f53f87300450010a6c12722db3fe103656828
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-13 20:34:17 +0900

    db-005 INSERT文大枠作成

---
+++ db/db-challenge3/db-005/db-005.sql
@@ -0,0 +1,12 @@
+INSERT INTO tasks
+    (chatroom_id,
+    content,
+    responsible_user_id,
+    limited_at,
+    is_completed,
+    is_deleted,
+    created_at,
+    created_user_id,
+    modified_at,
+    modified_user_id)
+VALUES()
