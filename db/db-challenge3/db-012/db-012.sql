5f2a9390b82776198f68f158f34654321712d2d9
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-25 14:52:17 +0900

    db-012 脱字の修正

--- db/db-challenge3/db-012/db-012.sql
+++ db/db-challenge3/db-012/db-012.sql
@@ -3,7 +3,7 @@ BEGIN;
     SET is_enabled_file_upload = 0, modified_at=NOW(), modified_user_id = 1
     WHERE chatrooms.id NOT IN
     (SELECT chatrooms_alias.id
-        FROM (SELECT idFROM chatrooms)AS chatrooms_alias
+        FROM (SELECT id FROM chatrooms)AS chatrooms_alias
         /*ERROR1093 回避のためのエイリアス*/
         JOIN chatroom_members ON chatrooms_alias.id = chatroom_members.chatroom_id
         WHERE chatroom_members.user_id=1)
