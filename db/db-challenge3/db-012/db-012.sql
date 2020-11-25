9315f82747fe4b0f542741f7f5c56c0394d9c30f
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-25 12:08:47 +0900

    db-012 作成

---
+++ db/db-challenge3/db-012/db-012.sql
@@ -0,0 +1,19 @@
+BEGIN;
+    UPDATE chatrooms
+    SET is_enabled_file_upload = 0, modified_at=NOW(), modified_user_id = 1
+    WHERE chatrooms.id NOT IN
+    (SELECT chatrooms_alias.id
+        FROM (SELECT id
+            FROM chatrooms)AS chatrooms_alias
+            JOIN chatroom_members ON chatrooms_alias.id = chatroom_members.chatroom_id
+        WHERE chatroom_members.user_id=1)
+        AND is_deleted = 0;
+    COMMIT;
+
+/*
+副問合せ用のメモ
+SELECT chatrooms.id, chatrooms.name, chatroom_members.user_id
+FROM chatrooms
+    JOIN chatroom_members ON chatrooms.id=chatroom_members.chatroom_id
+WHERE user_id=1;
+*/
