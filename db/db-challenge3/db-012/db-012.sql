08e369563516aea69bda5b88d069a41b009b44c0
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-28 14:38:32 +0900

    db-012 コメントの修正

--- db/db-challenge3/db-012/db-012.sql
+++ db/db-challenge3/db-012/db-012.sql
@@ -6,27 +6,6 @@ BEGIN;
     COMMIT;
 
 /*
-副問合せ用のメモ
-SELECT chatrooms.id, chatrooms.name, chatroom_members.user_id
-FROM chatrooms
-    JOIN chatroom_members ON chatrooms.id=chatroom_members.chatroom_id
-WHERE user_id=1;
-*/
-
-/*
-修正前の命令文
-UPDATE chatrooms
-    SET is_enabled_file_upload = 0, modified_at=NOW(), modified_user_id = 1
-    WHERE chatrooms.id NOT IN
-    (SELECT chatrooms_alias.id
-    FROM (SELECT id
-        FROM chatrooms)AS chatrooms_alias --ERROR1093 回避のためのエイリアス
-        JOIN chatroom_members ON chatrooms_alias.id = chatroom_members.chatroom_id
-    WHERE chatroom_members.user_id=1)
-    AND is_deleted = 0;
-*/
-
-/*
 ERROR1093回避のために試した命令文
     BEGIN;
         UPDATE chatrooms
@@ -38,7 +17,19 @@ ERROR1093回避のために試した命令文
             WHERE chatroom_members.user_id=1)
             AND is_deleted = 0;
     COMMIT;
-
 実行結果
     #1093 - FROM句にある表 'chatrooms' はUPDATEの対象にできません。
+*/
+
+/*
+前回提出した命令文
+UPDATE chatrooms
+    SET is_enabled_file_upload = 0, modified_at=NOW(), modified_user_id = 1
+    WHERE chatrooms.id NOT IN
+    (SELECT chatrooms_alias.id
+    FROM (SELECT id
+        FROM chatrooms)AS chatrooms_alias --ERROR1093 回避のためのエイリアス
+        JOIN chatroom_members ON chatrooms_alias.id = chatroom_members.chatroom_id
+    WHERE chatroom_members.user_id=1)
+    AND is_deleted = 0;
 */
