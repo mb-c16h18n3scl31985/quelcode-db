c960cede148da5ede0ed4dca6baae0b1b6b512ef
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-28 10:29:13 +0900

    db-011 UPDATE対象modified_user_idの追加

--- db/db-challenge3/db-011/db-011.sql
+++ db/db-challenge3/db-011/db-011.sql
@@ -1,6 +1,6 @@
 BEGIN;
     UPDATE tasks
-    SET is_completed = 1, modified_at = NOW()
+    SET is_completed = 1, modified_at = NOW(),modifed_user_id=1
     WHERE created_at BETWEEN '2020-04-26T09:00:00' AND '2020-04-26T11:30:00'
         AND is_deleted = 0
         AND created_user_id = 1;
