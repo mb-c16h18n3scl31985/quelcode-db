2dfde41f89f3aec2f504f390175391ddfa134660
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-28 12:50:57 +0900

    db-014 修正

--- db/db-challenge3/db-014/db-014.sql
+++ db/db-challenge3/db-014/db-014.sql
@@ -1,10 +1,19 @@
 BEGIN;
+    SELECT *
+    FROM chatroom_members
+        LEFT JOIN chatrooms ON chatrooms.id=chatroom_members.chatroom_id
+        LEFT JOIN users ON users.id=chatroom_members.user_id
+    WHERE chatrooms.is_deleted = 1
+        AND users.is_deleted=1;
+    COMMIT;
+
+/*
+BEGIN;
     DELETE FROM chatroom_members
     WHERE chatroom_members.chatroom_id IN
         (SELECT chatrooms.id
         FROM (SELECT chatroom_members.chatroom_id, chatroom_members.user_id FROM chatroom_members)
-            AS chatroom_members_alias
-            /*ERROR 1093回避のためのエイリアス*/
+            AS chatroom_members_alias --ERROR 1093回避のためのエイリアス
         JOIN chatrooms ON chatroom_members_alias.chatroom_id = chatrooms.id
         JOIN users ON chatroom_members_alias.user_id = users.id
         WHERE chatrooms.is_deleted = 1
@@ -12,13 +21,13 @@ BEGIN;
     AND chatroom_members.user_id IN
         (SELECT users.id
         FROM (SELECT chatroom_members.chatroom_id, chatroom_members.user_id FROM chatroom_members)
-            AS chatroom_members_alias
-            /*ERROR 1093回避のためのエイリアス*/
+            AS chatroom_members_alias --ERROR 1093回避のためのエイリアス
         JOIN chatrooms ON chatroom_members_alias.chatroom_id = chatrooms.id
         JOIN users ON chatroom_members_alias.user_id = users.id
         WHERE chatrooms.is_deleted = 1
         AND users.is_deleted = 1);
 COMMIT;
+*/
 
 /*
 副問合せ用のメモ
@@ -28,4 +37,13 @@ FROM chatroom_members
     JOIN users ON chatroom_members.user_id = users.id
 WHERE chatrooms.is_deleted=1
     AND users.is_deleted=1;
+*/
+
+/*
+BEGIN;
+SET FOREIGN_KEY_CHECKS = 0;
+DELETE FROM chatroom_members
+WHERE user_id =(SELECT id FROM users WHERE is_deleted=1);
+SET FOREIGN_KEY_CHECKS = 1;
+COMMIT;
 */
