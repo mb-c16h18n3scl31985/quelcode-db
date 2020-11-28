7b9d20ad3fea54e64cecadfff1124d275f061e23
yumi takahashi <c16h18n3scl31985@gmail.com>
2020-11-28 10:30:56 +0900

    db-011 不要なWHERE条件の削除

--- db/db-challenge3/db-011/db-011.sql
+++ db/db-challenge3/db-011/db-011.sql
@@ -2,6 +2,5 @@ BEGIN;
     UPDATE tasks
     SET is_completed = 1, modified_at = NOW(),modified_user_id=1
     WHERE created_at BETWEEN '2020-04-26T09:00:00' AND '2020-04-26T11:30:00'
-        AND is_deleted = 0
-        AND created_user_id = 1;
+        AND is_deleted = 0;
     COMMIT;
